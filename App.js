4f7474a4dca9a0edfad695ebeeb085f801264ea8
Chase <cabbott93@gmail.com>
2021-07-07 11:56:39 -0700

    images rendering

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 //useEffect
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 
 const URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
 export default function App() {
   const [pokemon, setPokemon] = useState([]);
 
+  useEffect(() => {
+    getPokemon();
+  }, [1])
+
   const getPokemon = () => {
     return fetch(URL, {
-      method: 'GET', // or 'PUT'
-      // headers: {
-      //   'Content-Type': 'application/json',
-      // },
-      // body: JSON.stringify(data),
+      method: 'GET',
     })
       .then(response => response.json())
-      .then(response => setPokemon(response))
-      .then(data => {
-        console.log('Success:', data);
+      // .then(data => {
+      //   console.log('Success:', data);
+      // })
+      .then(response => {
+        return setPokemon(response.results)
       })
       .catch((error) => {
         console.error('Error:', error);
       });
   }
-  getPokemon();
+
   return (
     <View style={styles.container}>
+      <FlatList
+        // style={[{ flex: 1 }]}
+        data={pokemon}
+        renderItem={({ item }) => {
+          return < Image source={{ uri: item.url_image }} style={styles.tinyLogo} />
+        }}
+      />
       <StatusBar style="auto" />
       <Text>Hello World!</Text>
     </View>
@@ -36,53 +45,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  }
 });
-
-// export class App extends Component {
-//   getPokemon () {
-//     const results = fetch(URL, {
-//   method: 'GET', // or 'PUT'
-//   // headers: {
-//   //   'Content-Type': 'application/json',
-//   // },
-//   // body: JSON.stringify(data),
-//   })
-//     .then(response => response.json())
-//     .then(response => setPokemon(response))
-//     .then(data => {
-//       // console.log('Success:', data);
-//     })
-//     .catch((error) => {
-//       // console.error('Error:', error);
-//     });
-//     setPokemon();
-//     console.log('THIS IS THE ARRAY!!', results);
-//   }
-
-//   componentDidMount() {
-
-//   }
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <StatusBar style="auto" />
-//         <Text>Hello World!</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
