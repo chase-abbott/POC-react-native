31e97847f823ad892e2933d8a2888d09472d2483
David Delgadillo <daviddel.731@gmail.com>
2021-07-07 09:58:43 -0700

    successfully fetching pokemon data using useState

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [pokemon, setPokemon] = useState([]);
+
+  // componentDidMount = () => {
+  //   this.getPokemon();
+  // }
+
+  const getPokemon = async () => {
+    const { results } = await fetch(URL, {
+  method: 'GET', // or 'PUT'
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // },
+  // body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    // .then(data => {
+    //   console.log('Success:', data);
+    // })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+    setPokemon(results);
+  }
+    getPokemon();
+      return (
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Text>Hello World!</Text>
+        </View>
+      );
+    }
 
 const styles = StyleSheet.create({
   container: {
